7898d49e8de855f7321a357053902d3aacf60849
ExcelWhite <excelwhite20@gmail.com>
2025-04-17 01:47:52 +0300

    Update fonts.py

--- extractors/fonts.py
+++ extractors/fonts.py
@@ -9,9 +9,12 @@ def extract_font_info(doc):
             if "lines" not in block:
                 continue
             for line in block["lines"]:
-                for span in line["spans"]:
-                    font_sizes.append(span["size"])
-                    font_names.append(span["font"])
-                    color = span.get("color", 0)
-                    font_colors.append("#{0:06x}".format(color))
+                line_text = ''.join(span['text'] for span in line['spans'])
+                if line_text.strip():  # This checks for non-empty and non-whitespace
+                    print(line_text)
+                    for span in line["spans"]:
+                        font_sizes.append(span["size"])
+                        font_names.append(span["font"])
+                        color = span.get("color", 0)
+                        font_colors.append("#{0:06x}".format(color))
     return font_sizes, font_names, font_colors
